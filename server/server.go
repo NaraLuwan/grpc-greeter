3d91f2ee0c8164c112ad82b050c96a6b0948043d
NaraLuwan <hqcadmin@163.com>
2021-08-30 20:56:34 +0800

    feat:add annotation

--- server/server.go
+++ server/server.go
@@ -26,7 +26,7 @@ func main() {
 	greeterServer := grpc.NewServer()
 	// 在gRPC服务端注册服务
 	pb.RegisterGreeterServer(greeterServer, &greeterImpl{})
-
+	// 注册grpcurl所需的reflection服务，使用grpcurl命令行工具可直接调用
 	reflection.Register(greeterServer)
 
 	// Serve方法在listener上接受传入连接，为每个连接创建一个ServerTransport和server的goroutine
